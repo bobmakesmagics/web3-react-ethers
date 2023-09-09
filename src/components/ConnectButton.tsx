fbe0166aba0d12f8fc1969769d66e7cf0087c852
bobmakesmagics <wildhorse1012@gmail.com>
2023-09-09 11:09:58 +0300

    replace web3.js with ethers.js and fixed errors

---
+++ src/components/ConnectButton.tsx
@@ -0,0 +1,357 @@
+import { useState, useEffect, useCallback } from 'react';
+import { BrowserProvider, Contract, parseEther } from 'ethers';
+import { useWeb3React } from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
+import { formatEther } from '@ethersproject/units';
+import {
+  Button,
+  Box,
+  Text,
+  Input,
+  Switch,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from '@chakra-ui/react';
+import { useDisclosure, useToast } from '@chakra-ui/react';
+import { UserRejectedRequestError } from '@web3-react/injected-connector';
+import { injected } from '../config/wallets';
+import abi from './abi.json';
+import { TailSpin } from 'react-loading-icons';
+
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
+
+export default function ConnectButton() {
+  const {
+    account,
+    active,
+    activate,
+    setError,
+    library,
+    deactivate,
+    connector,
+  } = useWeb3React<Web3Provider>();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [connected, setConnected] = useState<boolean>(false);
+  const [balance, setBalance] = useState<string>('');
+  const [babyBalance, setBabyBalance] = useState<string>('0');
+  const [mode, setMode] = useState<string>('BNB');
+  const [recieverAdd, setRecieverAdd] = useState<string>('');
+  const [sendAmount, setSendAmount] = useState<number>(0);
+  const [gasFee, setGasFee] = useState<string>('');
+  const [gasLimit, setGasLimit] = useState<number>(0);
+  const [loading, setLoading] = useState(false);
+  const toast = useToast();
+
+  const addressContract = '0xc748673057861a797275CD8A068AbB95A902e8de';
+
+  function handleConnectWallet() {
+    connected
+      ? deactivate()
+      : activate(
+          injected,
+          (error) => {
+            if (error instanceof UserRejectedRequestError) {
+              console.log('user refused');
+            } else {
+              setError(error);
+            }
+          },
+          false
+        );
+    setConnected(!connected);
+  }
+
+  function handleMode() {
+    setMode(mode === 'BNB' ? 'BabyDoge' : 'BNB');
+  }
+
+  function handleChangeAddress(event: any) {
+    setRecieverAdd(event.target.value);
+  }
+
+  function handleChangeAmount(event: any) {
+    setSendAmount(event.target.value);
+  }
+
+  async function handleOpenModal() {
+    if (!recieverAdd) {
+      return toast({
+        description: 'Please input Receiver Address',
+        status: 'error',
+      });
+    }
+    if (!sendAmount || sendAmount === 0) {
+      return toast({
+        description: 'Please input send amount',
+        status: 'error',
+      });
+    }
+    if (window.ethereum) {
+      let tx = {
+        to: recieverAdd,
+        value: parseEther(sendAmount.toString()),
+      };
+      const provider = new BrowserProvider(window.ethereum);
+      const gasPrice = await provider.estimateGas(tx);
+
+      setGasFee(gasPrice.toString());
+
+      const block = await provider.getBlock('latest');
+      const limit = block?.gasLimit as BigInt;
+      setGasLimit(parseInt(limit?.toString()));
+    }
+    onOpen();
+  }
+
+  const sendBaby = useCallback(async () => {
+    if (active && account && connector) {
+      const provider = new BrowserProvider(window.ethereum);
+      const erc20 = new Contract(
+        addressContract,
+        abi,
+        await provider.getSigner()
+      );
+      try {
+        await erc20.transfer(recieverAdd, parseEther(sendAmount.toString()));
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, [account, recieverAdd, sendAmount]);
+
+  const sendAction = useCallback(async () => {
+    if (window.ethereum) {
+      setLoading(true);
+      let txParams = {
+        to: recieverAdd,
+        value: parseEther(sendAmount.toString()),
+      };
+      const provider = new BrowserProvider(window.ethereum);
+      const signer = await provider.getSigner();
+
+      try {
+        const tx = await signer.sendTransaction(txParams);
+        onClose();
+        await tx.wait();
+        valueload();
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+        setLoading(false);
+      }
+    }
+  }, [recieverAdd, sendAmount]);
+
+  const valueload = useCallback(async () => {
+    if (active && account && library) {
+      library?.getBalance(account).then((result) => {
+        setBalance(result ? Number(formatEther(result)).toFixed(5) : '0');
+      });
+
+      const provider = new BrowserProvider(window.ethereum);
+      const contract = new Contract(addressContract, abi, provider);
+      await contract
+        .balanceOf(account)
+        .then((result: string) => {
+          setBabyBalance(
+            result ? parseFloat(formatEther(result)).toFixed(1) : '0'
+          );
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [account, library, active]);
+
+  useEffect(() => {
+    if (!active) return;
+    valueload();
+  }, [active]);
+
+  return (
+    <>
+      <h1 className="title">Metamask login demo from Enva Division</h1>
+      {account ? (
+        <Box
+          display="block"
+          alignItems="center"
+          background="white"
+          borderRadius="xl"
+          p="4"
+          width="300px"
+        >
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            mb="2"
+          >
+            <Text color="#158DE8" fontWeight="medium">
+              Account:
+            </Text>
+            <Text color="#6A6A6A" fontWeight="medium">
+              {`${account.slice(0, 6)}...${account.slice(
+                account.length - 4,
+                account.length
+              )}`}
+            </Text>
+          </Box>
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            mb="2"
+          >
+            <Text color="#158DE8" fontWeight="medium">
+              BabyDoge Balance :
+            </Text>
+            <Text color="#6A6A6A" fontWeight="medium">
+              {babyBalance}
+            </Text>
+          </Box>
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            mb="2"
+          >
+            <Text color="#158DE8" fontWeight="medium">
+              BNB Balance:
+            </Text>
+            <Text color="#6A6A6A" fontWeight="medium">
+              {balance}
+            </Text>
+          </Box>
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            mb="2"
+          >
+            <Text color="#158DE8" fontWeight="medium">
+              BNB / BabyDoge
+            </Text>
+            <Switch size="md" value={mode} onChange={handleMode} />
+          </Box>
+          <Box
+            display="block"
+            justifyContent="space-between"
+            alignItems="center"
+            mb="4"
+          >
+            <Text color="#158DE8" fontWeight="medium">
+              Send {mode}:
+            </Text>
+            <Input
+              bg="#EBEBEB"
+              size="lg"
+              value={recieverAdd}
+              onChange={handleChangeAddress}
+            />
+          </Box>
+          <Box display="flex" alignItems="center" mb="4">
+            <Input
+              bg="#EBEBEB"
+              size="lg"
+              value={sendAmount}
+              onChange={handleChangeAmount}
+            />
+            <Button
+              onClick={handleOpenModal}
+              bg="#158DE8"
+              color="white"
+              fontWeight="medium"
+              borderRadius="xl"
+              ml="2"
+              border="1px solid transparent"
+              _hover={{
+                borderColor: 'blue.700',
+                color: 'gray.800',
+              }}
+              _active={{
+                backgroundColor: 'blue.800',
+                borderColor: 'blue.700',
+              }}
+            >
+              {loading ? <TailSpin /> : 'Send'}
+            </Button>
+          </Box>
+          <Box display="flex" justifyContent="center" alignItems="center">
+            <Button
+              onClick={handleConnectWallet}
+              bg="#158DE8"
+              color="white"
+              fontWeight="medium"
+              borderRadius="xl"
+              border="1px solid transparent"
+              width="300px"
+              _hover={{
+                borderColor: 'blue.700',
+                color: 'gray.800',
+              }}
+              _active={{
+                backgroundColor: 'blue.800',
+                borderColor: 'blue.700',
+              }}
+            >
+              Disconnect Wallet
+            </Button>
+          </Box>
+          <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Are you Sure?</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <div>
+                  Are you sure {sendAmount} {mode} to {recieverAdd} user?
+                </div>
+                <div>Gas Limit: {gasLimit}</div>
+                <div>Gas Price: {gasFee}</div>
+              </ModalBody>
+              <ModalFooter>
+                <Button colorScheme="blue" mr={3} onClick={onClose}>
+                  Close
+                </Button>
+                <Button variant="ghost" onClick={sendAction}>
+                  Send
+                </Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+        </Box>
+      ) : (
+        <Box bg="white" p="4" borderRadius="xl">
+          <Button
+            onClick={handleConnectWallet}
+            bg="#158DE8"
+            color="white"
+            fontWeight="medium"
+            borderRadius="xl"
+            border="1px solid transparent"
+            width="300px"
+            _hover={{
+              borderColor: 'blue.700',
+              color: 'gray.800',
+            }}
+            _active={{
+              backgroundColor: 'blue.800',
+              borderColor: 'blue.700',
+            }}
+          >
+            Connect Wallet
+          </Button>
+        </Box>
+      )}
+    </>
+  );
+}
